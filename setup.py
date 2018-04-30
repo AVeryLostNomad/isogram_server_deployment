124feb3ea2aea6b0ddf68088ac0e5fab80172cfc
AVeryLostNomad <joey.e.maher-1@ou.edu>
2018-04-30 14:09:32 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='isogram-server',
+    version='1.0',
+    description='just an app',
+    author='me',
+    author_email='me@gmail.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=['Flask==0.12.2','numpy==1.14.0'],
+)
